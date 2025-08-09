be14e399b2ef818da6ed69588af9dd5f17ece60f
eg <yveltal3434@gmail.com>
2025-08-08 21:40:33 -0400

    decompile code and make patches

---
+++ PatchThingy/DataFile.cs
@@ -0,0 +1,34 @@
+using UndertaleModLib;
+using UndertaleModLib.Models;
+using UndertaleModLib.Decompiler;
+using CodeChicken.DiffPatch;
+using Underanalyzer.Decompiler;
+
+class DataFile
+{
+    string filePath = "/home/eg/.local/share/Steam/steamapps/common/DELTARUNE/chapter2_windows";
+    public UndertaleData Data;
+
+        GlobalDecompileContext globalDecompileContext;
+        IDecompileSettings decompilerSettings;
+
+    public DataFile(string fileName)
+    {
+        using (Stream file = File.Open(Path.Combine(filePath, fileName), FileMode.Open))
+        {
+            Data = UndertaleIO.Read(file);
+        }
+
+        globalDecompileContext = new(Data);
+        decompilerSettings = Data.ToolInfo.DecompilerSettings;
+    }
+
+    public List<string> DecompileCode(UndertaleCode code)
+    {
+        string codeText = new DecompileContext(globalDecompileContext, code, decompilerSettings).DecompileToString();
+
+        return codeText
+            .Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+            .ToList();
+    }
+}
--- PatchThingy/Program.cs
+++ PatchThingy/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// run this with `dotnet run` or pressing F5
+
+using UndertaleModLib;
+using UndertaleModLib.Models;
+using UndertaleModLib.Decompiler;
+using CodeChicken.DiffPatch;
+
+DataFile vanilla = new("data-vanilla.win");
+DataFile modded = new("data.win");
+
+Console.WriteLine(vanilla.Data.GeneralInfo?.DisplayName?.Content.ToLower());
+Console.WriteLine(modded.Data.GeneralInfo?.DisplayName?.Content.ToLower());
+
+foreach (UndertaleCode modCode in modded.Data.Code)
+{
+    if (modCode.ParentEntry is not null)
+        continue;
+
+    UndertaleCode vanillaCode = vanilla.Data.Code.ByName(modCode.Name.Content);
+
+    if (vanillaCode is not null && vanillaCode.ParentEntry is null)
+    {
+        LineMatchedDiffer differ = new();
+        PatchFile modChanges = new();
+
+        modChanges.patches = differ.MakePatches(vanilla.DecompileCode(vanillaCode), modded.DecompileCode(modCode));
+        Console.WriteLine(modChanges.ToString());
+    }
+}
