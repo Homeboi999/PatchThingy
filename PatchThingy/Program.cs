b70476e8e0acb373171c38838b0374a49aa572a0
eg <yveltal3434@gmail.com>
2025-08-08 22:54:15 -0400

    write source files for added code

--- PatchThingy/Program.cs
+++ PatchThingy/Program.cs
@@ -5,11 +5,13 @@ using UndertaleModLib;
 using UndertaleModLib.Models;
 using UndertaleModLib.Decompiler;
 using CodeChicken.DiffPatch;
+using System.Text.Json;
 
 DataFile vanilla = new("data-vanilla.win");
 DataFile modded = new("data.win");
 
 Directory.CreateDirectory("./Patches/Code");
+Directory.CreateDirectory("./Source/Code");
 foreach (UndertaleCode modCode in modded.Data.Code)
 {
     if (modCode.ParentEntry is not null)
@@ -31,5 +33,22 @@ foreach (UndertaleCode modCode in modded.Data.Code)
 
         File.WriteAllText($"./Patches/Code/{modCode.Name.Content}.gml.patch", modChanges.ToString());
         Console.WriteLine($"Created .patch file for {modCode.Name.Content}.gml");
+    }
+    else if (vanillaCode is null)
+    {
+        File.WriteAllLines($"./Source/Code/{modCode.Name.Content}.gml", modded.DecompileCode(modCode));
+        Console.WriteLine($"Created source file for {modCode.Name.Content}.gml");
+    }
+}
+
+Directory.CreateDirectory("./Source/Scripts");
+foreach (UndertaleScript modScript in modded.Data.Scripts)
+{
+    UndertaleScript vanillaScript = vanilla.Data.Scripts.ByName(modScript.Name.Content);
+
+    if (vanillaScript is null)
+    {
+        File.WriteAllText($"./Source/Scripts/{modScript.Name.Content}.json", JsonSerializer.Serialize(new ScriptDefinition(modScript.Name.Content, modScript.Code.Name.Content)));
+        Console.WriteLine($"Created script definition for {modScript.Name.Content}");
     }
 }
---
+++ PatchThingy/ScriptDefinition.cs
@@ -0,0 +1,10 @@
+using UndertaleModLib;
+using UndertaleModLib.Models;
+using UndertaleModLib.Decompiler;
+using CodeChicken.DiffPatch;
+using Underanalyzer.Decompiler;
+
+public record ScriptDefinition (string Name, string Code)
+{
+    
+}
